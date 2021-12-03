a47d9c1c39125431fe27ff2c37bfd57a5fe4254c
jdgdoms <jessica.deguzman.domer@gmail.com>
2021-12-03 22:28:46 +0800

    button2
    asasasa

--- WindowsApplication1/WindowsApplication1/Form1.Designer.vb
+++ WindowsApplication1/WindowsApplication1/Form1.Designer.vb
@@ -22,65 +22,29 @@ Partial Class Form1
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        Me.Button1 = New System.Windows.Forms.Button()
-        Me.Licensing1 = New Guna.UI2.Licensing.Licensing()
-        Me.TextBox1 = New System.Windows.Forms.TextBox()
-        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Button2 = New System.Windows.Forms.Button()
         Me.SuspendLayout()
         '
-        'Button1
+        'Button2
         '
-        Me.Button1.Location = New System.Drawing.Point(99, 74)
-        Me.Button1.Name = "Button1"
-        Me.Button1.Size = New System.Drawing.Size(75, 23)
-        Me.Button1.TabIndex = 0
-        Me.Button1.Text = "Close"
-        Me.Button1.UseVisualStyleBackColor = True
-        '
-        'Licensing1
-        '
-        Me.Licensing1.BackColor = System.Drawing.Color.White
-        Me.Licensing1.Location = New System.Drawing.Point(230, -20)
-        Me.Licensing1.MaximumSize = New System.Drawing.Size(348, 485)
-        Me.Licensing1.MinimumSize = New System.Drawing.Size(348, 485)
-        Me.Licensing1.Name = "Licensing1"
-        Me.Licensing1.Size = New System.Drawing.Size(348, 485)
-        Me.Licensing1.TabIndex = 1
-        '
-        'TextBox1
-        '
-        Me.TextBox1.Location = New System.Drawing.Point(66, 213)
-        Me.TextBox1.Name = "TextBox1"
-        Me.TextBox1.Size = New System.Drawing.Size(100, 20)
-        Me.TextBox1.TabIndex = 2
-        '
-        'Label1
-        '
-        Me.Label1.AutoSize = True
-        Me.Label1.Location = New System.Drawing.Point(82, 181)
-        Me.Label1.Name = "Label1"
-        Me.Label1.Size = New System.Drawing.Size(44, 13)
-        Me.Label1.TabIndex = 3
-        Me.Label1.Text = "VANNY"
+        Me.Button2.Location = New System.Drawing.Point(104, 71)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(75, 23)
+        Me.Button2.TabIndex = 0
+        Me.Button2.Text = "Button2"
+        Me.Button2.UseVisualStyleBackColor = True
         '
         'Form1
         '
-        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
-        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(575, 429)
-        Me.Controls.Add(Me.Label1)
-        Me.Controls.Add(Me.TextBox1)
-        Me.Controls.Add(Me.Licensing1)
-        Me.Controls.Add(Me.Button1)
+        Me.ClientSize = New System.Drawing.Size(284, 261)
+        Me.Controls.Add(Me.Button2)
         Me.Name = "Form1"
-        Me.Text = "Form1"
         Me.ResumeLayout(False)
-        Me.PerformLayout()
 
     End Sub
     Friend WithEvents Button1 As System.Windows.Forms.Button
-    Friend WithEvents Licensing1 As Guna.UI2.Licensing.Licensing
     Friend WithEvents TextBox1 As System.Windows.Forms.TextBox
     Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents Button2 As System.Windows.Forms.Button
 
 End Class
--- WindowsApplication1/WindowsApplication1/Form1.vb
+++ WindowsApplication1/WindowsApplication1/Form1.vb
@@ -7,4 +7,12 @@
     Private Sub Label1_Click(sender As Object, e As EventArgs) Handles Label1.Click
 
     End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+
+    End Sub
 End Class
